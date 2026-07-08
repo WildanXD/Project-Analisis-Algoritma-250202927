22751846823b8e859320628d65143d7a22f02316
250202927 <wildankocak90@gmail.com>
2026-07-08 22:30:36 +0700

    Delete Project Akhir/code directory

--- Project Akhir/code/Model.py
+++
@@ -1,8 +0,0 @@
-class Buku:
-    def __init__(self, kode, judul, penulis, kategori, tahun, status):
-        self.kode = kode
-        self.judul = judul
-        self.penulis = penulis
-        self.kategori = kategori
-        self.tahun = tahun
-        self.status = status
